ac1e6f0951e13b375578330a7354522d9ce85846
macie <maciek.zok+github@gmail.com>
2014-04-10 22:37:28 +0200

    it works!

--- unittest.sh
+++ unittest.sh
@@ -9,23 +9,23 @@
 
 
 #
-#   IMPORTS
-#
-
-source "plugins/assert.sh"
-source "plugins/stub.sh"
-
-
-#
 #   DEFAULTS
 #
 
-_VERSION_="0.1-dev"
+readonly _version_='0.1-dev'
 
-_COVERAGE=0  # no coverage
-_COVER_DIR=""
-_VERBOSITY=1  # normal verbosity
-_TEST_DIR=""
+_coverage=0  # no coverage
+_cover_dir=''
+_verbosity=1  # normal verbosity
+
+_test_dir=''
+_test_files=''
+
+_tests_run=0
+_tests_failed=0
+_fail_messages=''
+_current_testcase=''
+_current_testsuite=''
 
 
 #
@@ -45,19 +45,19 @@ unittest::help_message() {
   # Returns:
   #     String message to standard output.
   #
-  echo "unittest.sh - unit tests framework for shell scripts."
-  echo "Usage:"
-  echo "  unittest.sh [options] [filename]"
+  echo 'unittest.sh - unit tests framework for shell scripts.'
+  echo 'Usage:'
+  echo '  unittest.sh [options] [filename]'
   echo 
-  echo "Options:"
-  echo "  -h, --help                      Show this help and exit."
-  echo "  -V, --version                   Show version number and exit."
-  echo "  -w, --where <directory>         Look for tests in this directory."
-  echo "  -v, --verbose                   Be more verbose."
-  echo "  -q, --quiet                     Be less verbose."
-  echo "      --with-coverage             Enable plugin coverage.sh."
-  echo "      --cover-dir <directory>     Restrict coverage output to"
-  echo "                                  selected directory."
+  echo 'Options:'
+  echo '  -h, --help                      Show this help and exit.'
+  echo '  -V, --version                   Show version number and exit.'
+  echo '  -w, --where <directory>         Look for tests in this directory.'
+  echo '  -v, --verbose                   Be more verbose.'
+  echo '  -q, --quiet                     Be less verbose.'
+  echo '      --with-coverage             Enable plugin coverage.sh.'
+  echo '      --cover-dir <directory>     Restrict coverage output to'
+  echo '                                  selected directory.'
 }
 
 unittest::version_message() {
@@ -65,7 +65,7 @@ unittest::version_message() {
   # Shows version message.
   #
   # Globals:
-  #     _VERSION_ - Version number.
+  #     _version_ - Version number.
   #
   # Arguments:
   #     None
@@ -73,10 +73,10 @@ unittest::version_message() {
   # Returns:
   #     String message to standard output.
   #
-  echo "unittest.sh "${_VERSION_};
+  echo "unittest.sh ${_version_}"
   echo
-  echo "Copyright (c) 2014 Maciej Żok"
-  echo "MIT License (http://opensource.org/licenses/MIT)"
+  echo 'Copyright (c) 2014 Maciej Żok'
+  echo 'MIT License (http://opensource.org/licenses/MIT)'
 }
 
 unittest::error_message() {
@@ -93,9 +93,245 @@ unittest::error_message() {
   #     String message to err output.
   #
   local msg
-  local "${@}"
+  local "$@"
 
-  echo "Error: "${msg}"." 1>&2
+  echo "Error: ${msg}." 1>&2
+}
+
+unittest::create_fail_message() {
+  #
+  # Creates fail message.
+  #
+  # Globals:
+  #     _current_testcase (str) - 
+  #     _current_testsuite (str) - 
+  #     _fail_messages (str) - 
+  #
+  # Arguments:
+  #     None.
+  #
+  # Returns:
+  #     None.
+  #
+  _fail_messages="${_fail_messages}$(printf "
+======================================================================
+ FAIL: ${_current_testcase} (${_current_testsuite})
+----------------------------------------------------------------------
+ 
+ ")"
+}
+
+unittest::fail_indicator() {
+  #
+  # Fail indicator.
+  #
+  # Globals:
+  #     _verbosity (int) - Verbosity level.
+  #
+  # Arguments:
+  #     None.
+  #
+  # Returns:
+  #     None.
+  #
+  if [[ ${_verbosity} == 1 ]]; then
+    echo -n 'E'
+  elif [[ ${_verbosity} == 2 ]]; then
+    echo 'error'
+  fi
+}
+
+unittest::pass_indicator() {
+  #
+  # Pass indicator.
+  #
+  # Globals:
+  #     _verbosity (int) - Verbosity level.
+  #
+  # Arguments:
+  #     None.
+  #
+  # Returns:
+  #     None.
+  #
+  if [[ ${_verbosity} == 1 ]]; then
+    echo -n '.'
+  elif [[ ${_verbosity} == 2 ]]; then
+    echo 'ok'
+  fi
+}
+
+unittest::testcase_indicator() {
+  #
+  # Pass indicator.
+  #
+  # Globals:
+  #     _current_testcase (str) - 
+  #     _current_testsuite (str) - 
+  #     _verbosity (int) - Verbosity level.
+  #
+  # Arguments:
+  #     None.
+  #
+  # Returns:
+  #     None.
+  #
+  if [[ ${_verbosity} == 2 ]]; then
+    echo -n "${_current_testcase} (${_current_testsuite}) ... "
+  fi
+}
+
+#
+#   ASSERTIONS
+#
+
+assertEqual() {
+  # assert::equal 0 0  => pass
+  local result="$1"
+  local expected="$2"
+
+  (( _tests_run++ ))
+  if [[ "${result}" = "${expected}" ]]; then
+    unittest::pass_indicator
+    return 0
+  else
+    (( _tests_failed++ ))
+    unittest::fail_indicator
+    unittest::create_fail_message
+    return 1
+  fi
+}
+
+assertNotEqual() {
+  # assert::not_equal 0 1  => pass
+  local result="$1"
+  local expected="$2"
+
+  (( _tests_run++ ))
+  if [[ "${result}" != "${expected}" ]]; then
+    unittest::pass_indicator
+    return 0
+  else
+    (( _tests_failed++ ))
+    unittest::fail_indicator
+    unittest::create_fail_message
+    return 1
+  fi
+}
+
+assertTrue() {
+  # assert::true 1  => pass
+  # assert::true ""  => fail
+  local result="$1"
+
+  (( _tests_run++ ))
+  if ([ "${result}" != "0" ]) && ([ "${result}" ]); then
+    unittest::pass_indicator
+    return 0
+  else
+    (( _tests_failed++ ))
+    unittest::fail_indicator
+    unittest::create_fail_message
+    return 1
+  fi
+}
+
+assertFalse() {
+  # assert::false 0  => pass
+  # assert::false ""  => pass
+  local result="$1"
+
+  (( _tests_run++ ))
+  if ([ "${result}" = "0" ]) || ([ ! "${result}" ]); then
+    unittest::pass_indicator
+    return 0
+  else
+    (( _tests_failed++ ))
+    unittest::fail_indicator
+    unittest::create_fail_message
+    return 1
+  fi
+}
+
+assertRaises() {
+  # assert:raises function 0
+  local result="$1"
+  local expected="$2"
+
+  (( _tests_run++ ))
+  if [[ "${result}" = "${expected}" ]]; then
+    unittest::pass_indicator
+    return 0
+  else
+    (( _tests_failed++ ))
+    unittest::fail_indicator
+    unittest::create_fail_message
+    return 1
+  fi
+}
+
+assertGreater() {
+  local result=$1
+  local expected=$2
+
+  (( _tests_run++ ))
+  if (( ${result} > ${expected} )); then
+    unittest::pass_indicator
+    return 0
+  else
+    (( _tests_failed++ ))
+    unittest::fail_indicator
+    unittest::create_fail_message
+    return 1
+  fi
+}
+
+assertGreaterEqual() {
+  local result=$1
+  local expected=$2
+
+  (( _tests_run++ ))
+  if (( ${result} >= ${expected} )); then
+    unittest::pass_indicator
+    return 0
+  else
+    (( _tests_failed++ ))
+    unittest::fail_indicator
+    unittest::create_fail_message
+    return 1
+  fi
+}
+
+assertLess() {
+  local result=$1
+  local expected=$2
+
+  (( _tests_run++ ))
+  if (( ${result} < ${expected} )); then
+    unittest::pass_indicator
+    return 0
+  else
+    (( _tests_failed++ ))
+    unittest::fail_indicator
+    unittest::create_fail_message
+    return 1
+  fi
+}
+
+assertLessEqual() {
+  local result=$1
+  local expected=$2
+
+  (( _tests_run++ ))
+  if (( ${result} <= ${expected} )); then
+    unittest::pass_indicator
+    return 0
+  else
+    (( _tests_failed++ ))
+    unittest::fail_indicator
+    unittest::create_fail_message
+    return 1
+  fi
 }
 
 
@@ -108,10 +344,10 @@ unittest::parse_args() {
   # Parses script parameters.
   #
   # Globals:
-  #     _COVERAGE (int) - Is coverage plugin enabled?
-  #     _COVER_DIR (str) - Directory for coverage.
-  #     _TEST_DIR (str) - Directory with tests.
-  #     _VERBOSITY (int) - Verbosity level.
+  #     _coverage (int) - Is coverage plugin enabled?
+  #     _cover_dir (str) - Directory for coverage.
+  #     _test_dir (str) - Directory with tests.
+  #     _verbosity (int) - Verbosity level.
   #
   # Arguments:
   #     params (str) - Script params.
@@ -119,7 +355,7 @@ unittest::parse_args() {
   # Returns:
   #     String message or nothing.
   #
-  while [[ $1 == -* ]]; do
+  while [[ "$1" == "-*" ]]; do
     case "$1" in
       -h|--help|-\?)
         unittest::help_message
@@ -128,10 +364,10 @@ unittest::parse_args() {
 
       -w|--where)
         if (($# > 1)); then
-          _TEST_DIR=$2
+          _test_dir="$2"
           shift 2
         else
-          unittest::error_message msg="no directory specified"
+          unittest::error_message msg='no directory specified'
           exit 1
         fi
         ;;
@@ -142,27 +378,27 @@ unittest::parse_args() {
         ;;
 
       -v|--verbose)
-        _VERBOSITY=2
+        _verbosity=2
         shift 2
         ;;
 
       -q|--quiet)
-        _VERBOSITY=0
+        _verbosity=0
         shift 2
         ;;
 
       --with-coverage)
-        _COVERAGE=1
+        _coverage=1
         shift 2
         ;;
 
       --cover-dir)
-        if [[ $# > 1 ]] && [[ ${_COVERAGE} == 1 ]]; then
-          _COVER_DIR=$2
+        if [[ $# > 1 ]] && [[ ${_coverage} == 1 ]]; then
+          _cover_dir="$2"
           shift 2
         else
-          unittest::error_message msg="no directory specified
-                                       or no coverage support enabled"
+          unittest::error_message msg='no directory specified
+                                       or no coverage support enabled'
           exit 1
         fi
         ;;
@@ -180,7 +416,7 @@ unittest::autodiscovery() {
   # Finds directories with tests.
   #
   # Globals:
-  #     _TEST_DIR (str) - Directory with tests.
+  #     _test_dir (str) - Directory with tests.
   #
   # Arguments:
   #     None
@@ -188,14 +424,11 @@ unittest::autodiscovery() {
   # Returns:
   #     testfiles (str) - Files with tests.
   #
-  if [[ ${_TEST_DIR} == "" ]]; then
-    _TEST_DIR=$(find $(pwd)/ -type d | grep "tests")
+  if [[ "${_test_dir}" == '' ]]; then
+    _test_dir=$(find $(pwd)/ -type d | grep -m 1 -E '/tests(/.*)?$')
   fi
 
-  testfiles=""
-  for testdir in ${_TEST_DIR}; do
-    testfiles=${testfiles}\n"$(find "${_TEST_DIR}" -name "test_*.sh")"
-  done
+  _test_files="$(find "${_test_dir}" -name "test_*.sh")"
 }
 
 unittest::start() {
@@ -203,15 +436,15 @@ unittest::start() {
   # Runs instruction before all tests.
   #
   # Globals:
-  #     None
-  #
-  # Arguments:
-  #     None
-  #
-  # Returns:
   #     start_time (str) - Start time of tests (in nanoseconds).
   #
-  start_time="$(date +%s%N)"  # in nanoseconds
+  # Arguments:
+  #     None.
+  #
+  # Returns:
+  #     None.
+  #
+  _start_time="$(date +%s%N)"  # in nanoseconds
 }
 
 unittest::stop() {
@@ -219,27 +452,31 @@ unittest::stop() {
   # Runs instruction after all tests.
   #
   # Globals:
-  #     None
+  #     _start_time (str) - Start time of tests (in nanoseconds).
+  #     _tests_run (str) - Number of tests.
   #
   # Arguments:
-  #     start_time (str) - Start time of tests (in nanoseconds).
-  #     tests_number (str) - Number of tests.
+  #     None.
   #
   # Returns:
   #     None.
   #
-  local tests_number start_time
-  local "${@}"
+  local end_status=""
+  local teststime=$(($(date +%s%N) - ${_start_time}))
 
-  local teststime=$(($(date +%s%N) - start_time))
+  if (( ${_tests_failed} > 0 )); then
+    printf "${_fail_messages}"
+    end_status="FAILED (failures=${_tests_failed})"
+  else
+    end_status='OK'
+  fi
 
-  # FIXME: use printf
-  echo "---------------------------------------------"
-  echo "Ran "${tests_number}" tests in "${teststime}"s"
-  echo
+  printf "
+----------------------------------------------------------------------
+Ran ${_tests_run} tests in ${teststime}s
 
-  echo "OK"
-  echo "ERROR"
+${end_status}
+"
 }
 
 unittest::before_test() {
@@ -247,22 +484,15 @@ unittest::before_test() {
   # Runs instruction before each test function.
   #
   # Globals:
-  #     _VERBOSITY (int) - Verbosity level.
+  #     None.
   #
   # Arguments:
-  #     path (str) - Test file name.
-  #     name (str) - Test function name.
+  #     None.
   #
   # Returns:
   #     None.
   #
-  local path name
-  local "${@}"
-
-  if [[ _VERBOSITY == 2 ]]; then
-    echo -n ${name}" ("${path}") ... "
-  fi
-
+  unittest::testcase_indicator
 }
 
 unittest::after_test() {
@@ -270,7 +500,7 @@ unittest::after_test() {
   # Runs instruction after each test function.
   #
   # Globals:
-  #     _VERBOSITY (int) - Verbosity level.
+  #     _verbosity (int) - Verbosity level.
   #
   # Arguments:
   #     None.
@@ -278,16 +508,7 @@ unittest::after_test() {
   # Returns:
   #     None.
   #
-
-  _assert_reset
-
-  if [[ _VERBOSITY == 1 ]]; then
-    echo -n "."
-    echo -n "E"
-  elif [[ _VERBOSITY == 2 ]]; then
-    echo "ok"
-    echo "error"
-  fi
+  return 0
 }
 
 unittest::testrunner() {
@@ -306,26 +527,33 @@ unittest::testrunner() {
 
   unittest::start
 
-  for testfile in $testfiles; do
+  for testfile in ${_test_files}; do
 
-    source testfile
+    _current_testsuite="${testfile#"$(pwd)/"}"
 
-    testfunctions=`grep -o "test_[^\(]*" ${testfile}`
-    setUp=`grep -o "setUp" ${testfile}`
-    tearDown=`grep -o "tearDown" ${testfile}`
+    source ${testfile}
+    local setup=$(grep -o "setUp" ${testfile})
+    local teardown=$(grep -o "tearDown" ${testfile})
+    local test_suite=$(grep -o "test_[^\(]*" ${testfile})
 
-    for testfunction in $testfunctions; do
-      unittest::before_test name=${testfunction} path=${testfile}
+    for test_case in ${test_suite}; do
 
-      setUp
-      testfunction
-      tearDown
-
+      _current_testcase="${test_case}"
+      unittest::before_test
+      ${setup}
+      ${test_case}
+      ${teardown}
       unittest::after_test
+
+      # reset test_case
+      unset -f ${test_case}
     done
+
+    # reset setup and teardown
+    unset -v ${setup} ${teardown}
   done
 
-  unittest::stop start_time=${start_time} tests_number=${tests_number}
+  unittest::stop
 }
 
 
@@ -334,11 +562,11 @@ unittest::testrunner() {
 #
 
 unittest::main () {
-  unittest::parse_args "${@}"
+  unittest::parse_args "$@"
   unittest::autodiscovery
   unittest::testrunner
 
   exit 0
 }
 
-unittest::main "${@}"
+unittest::main "$@"
