0db63b3e5710caf28b4e2a34f7d1191b40f7b24b
macie <macie@users.noreply.github.com>
2023-02-05 08:57:43 +0100

    refactor: Remove unnecessary subshell

--- tests/test_number_assertions.sh
+++ tests/test_number_assertions.sh
@@ -2,186 +2,186 @@
 
 
 test_equal_ints() {
-    `test 11 -eq 11`
+    test 11 -eq 11
 
     test $? -eq 0
 }
 
 test_equal_ints_false() {
-    `test 11 -eq 5 2>/dev/null`
+    test 11 -eq 5 2>/dev/null
 
     test $? -eq 1
 }
 
 test_equal_strings() {
-    `test 'a' -eq 'a' 2>/dev/null`
+    test 'a' -eq 'a' 2>/dev/null
 
     test $? -eq 1
 }
 
 test_equal_mixed() {
-    `test 'a' -eq 1 2>/dev/null`
+    test 'a' -eq 1 2>/dev/null
 
     test $? -eq 1
 }
 
 test_equal_mixed2() {
-    `test 2 -eq 'b' 2>/dev/null`
+    test 2 -eq 'b' 2>/dev/null
 
     test $? -eq 1
 }
 
 
 test_notequal_ints() {
-    `test 432 -ne 0`
+    test 432 -ne 0
 
     test $? -eq 0
 }
 
 test_notequal_ints_false() {
-    `test 7 -ne 7 2>/dev/null`
+    test 7 -ne 7 2>/dev/null
 
     test $? -eq 1
 }
 
 test_notequal_strings() {
-    `test 'a' -ne 'b' 2>/dev/null`
+    test 'a' -ne 'b' 2>/dev/null
 
     test $? -eq 1
 }
 
 test_notequal_mixed() {
-    `test 'a' -ne 1 2>/dev/null`
+    test 'a' -ne 1 2>/dev/null
 
     test $? -eq 1
 }
 
 test_notequal_mixed2() {
-    `test 2 -ne 'b' 2>/dev/null`
+    test 2 -ne 'b' 2>/dev/null
 
     test $? -eq 1
 }
 
 
 test_greaterthan_ints() {
-    `test 432 -gt 0`
+    test 432 -gt 0
 
     test $? -eq 0
 }
 
 test_greaterthan_ints_false() {
-    `test 7 -gt 7 2>/dev/null`
+    test 7 -gt 7 2>/dev/null
 
     test $? -eq 1
 }
 
 test_greaterthan_strings() {
-    `test 'a' -gt 'b' 2>/dev/null`
+    test 'a' -gt 'b' 2>/dev/null
 
     test $? -eq 1
 }
 
 test_greaterthan_mixed() {
-    `test 'aa' -gt 1 2>/dev/null`
+    test 'aa' -gt 1 2>/dev/null
 
     test $? -eq 1
 }
 
 test_greaterthan_mixed2() {
-    `test 2000 -gt 'b' 2>/dev/null`
+    test 2000 -gt 'b' 2>/dev/null
 
     test $? -eq 1
 }
 
 
 test_greaterequal_ints() {
-    `test 0 -ge -1`
+    test 0 -ge -1
 
     test $? -eq 0
 }
 
 test_greaterequal_ints_false() {
-    `test 6 -ge 7 2>/dev/null`
+    test 6 -ge 7 2>/dev/null
 
     test $? -eq 1
 }
 
 test_greaterequal_strings() {
-    `test 'a' -ge 'a' 2>/dev/null`
+    test 'a' -ge 'a' 2>/dev/null
 
     test $? -eq 1
 }
 
 test_greaterequal_mixed() {
-    `test 'a' -ge 1 2>/dev/null`
+    test 'a' -ge 1 2>/dev/null
 
     test $? -eq 1
 }
 
 test_greaterequal_mixed2() {
-    `test 2 -ge 'b' 2>/dev/null`
+    test 2 -ge 'b' 2>/dev/null
 
     test $? -eq 1
 }
 
 
 test_lessthan_ints() {
-    `test -2 -lt -1`
+    test -2 -lt -1
 
     test $? -eq 0
 }
 
 test_lessthan_ints_false() {
-    `test 7 -lt 7 2>/dev/null`
+    test 7 -lt 7 2>/dev/null
 
     test $? -eq 1
 }
 
 test_lessthan_strings() {
-    `test 'a' -lt 'bb' 2>/dev/null`
+    test 'a' -lt 'bb' 2>/dev/null
 
     test $? -eq 1
 }
 
 test_lessthan_mixed() {
-    `test 'a' -lt 100 2>/dev/null`
+    test 'a' -lt 100 2>/dev/null
 
     test $? -eq 1
 }
 
 test_lessthan_mixed2() {
-    `test -2 -lt 'b' 2>/dev/null`
+    test -2 -lt 'b' 2>/dev/null
 
     test $? -eq 1
 }
 
 
 test_lessequal_ints() {
-    `test 0 -le 0`
+    test 0 -le 0
 
     test $? -eq 0
 }
 
 test_lessequal_ints_false() {
-    `test 7 -le 6 2>/dev/null`
+    test 7 -le 6 2>/dev/null
 
     test $? -eq 1
 }
 
 test_lessequal_strings() {
-    `test 'a' -le 'b' 2>/dev/null`
+    test 'a' -le 'b' 2>/dev/null
 
     test $? -eq 1
 }
 
 test_lessequal_mixed() {
-    `test 'a' -le 1111 2>/dev/null`
+    test 'a' -le 1111 2>/dev/null
 
     test $? -eq 1
 }
 
 test_lessequal_mixed2() {
-    `test 2 -le 'zzz' 2>/dev/null`
+    test 2 -le 'zzz' 2>/dev/null
 
     test $? -eq 1
 }
--- tests/test_string_assertions.sh
+++ tests/test_string_assertions.sh
@@ -2,119 +2,119 @@
 
 
 test_same_strings() {
-    `test 'abc' = 'abc'`
+    test 'abc' = 'abc'
 
     test $? -eq 0
 }
 
 test_same_strings_false() {
-    `test 'abc' = 'abb' 2>/dev/null`
+    test 'abc' = 'abb' 2>/dev/null
 
     test $? -eq 1
 }
 
 test_same_strings_mixed() {
-    `test '1' = 1`
+    test '1' = 1
 
     test $? -eq 0
 }
 
 test_same_strings_mixed2() {
-    `test 1 = '1'`
+    test 1 = '1'
 
     test $? -eq 0
 }
 
 test_same_strings_ints() {
-    `test 123 = 123`
+    test 123 = 123
 
     test $? -eq 0
 }
 
 
 test_different_strings() {
-    `test 'a' != 'b'`
+    test 'a' != 'b'
 
     test $? -eq 0
 }
 
 test_different_strings_false() {
-    `test 'a' != 'a' 2>/dev/null`
+    test 'a' != 'a' 2>/dev/null
 
     test $? -eq 1
 }
 
 test_different_strings_mixed() {
-    `test 'a' != 32`
+    test 'a' != 32
 
     test $? -eq 0
 }
 
 test_different_strings_mixed2() {
-    `test 32 != '2'`
+    test 32 != '2'
 
     test $? -eq 0
 }
 
 test_different_strings_ints() {
-    `test 123 != 124`
+    test 123 != 124
 
     test $? -eq 0
 }
 
 
 test_empty_string() {
-    `test -z ''`
+    test -z ''
 
     test $? -eq 0
 }
 
 test_empty_string_false() {
-    `test -z ' ' 2>/dev/null`
+    test -z ' ' 2>/dev/null
 
     test $? -eq 1
 }
 
 test_empty_int() {
-    `test -z 0 2>/dev/null`
+    test -z 0 2>/dev/null
 
     test $? -eq 1
 }
 
 
 test_nonempty_string() {
-    `test -n ' '`
+    test -n ' '
 
     test $? -eq 0
 }
 
 test_nonempty_string_false() {
-    `test -n '' 2>/dev/null`
+    test -n '' 2>/dev/null
 
     test $? -eq 1
 }
 
 test_nonempty_int() {
-    `test -n 0`
+    test -n 0
 
     test $? -eq 0
 }
 
 
 test_notnull_string() {
-    `test ' '`
+    test ' '
 
     test $? -eq 0
 }
 
 test_notnull_string_false() {
-    `test '' 2>/dev/null`
+    test '' 2>/dev/null
 
     test $? -eq 1
 }
 
 test_notnull_int() {
-    `test 1`
+    test 1
 
     test $? -eq 0
 }
