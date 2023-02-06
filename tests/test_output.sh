macie/unittest.sh

commit efdc06f69deba1a33dd8edc4ce7deb883166eb4f

refactor: Remove shellcheck warnings
Fixed:
- SC1090 Can't follow non-constant source.
- SC2013 To read lines rather than words, pipe/redirect to a while read loop.
- SC2046 Quote this to prevent word splitting.
- SC2059 Don't use variables in the printf format string.
- SC2086 Double quote to prevent globbing and word splitting.
- SC2181 Check exit code directly.

unittest
@@ -63,7 +63,7 @@ unittest__print_debug() {
     shift 1
     for utt13o_paragraph in "$@"; do
         case ${utt13o_paragraph} in
-            ' '*) printf "${utt13o_color_quote}" >&2 ;;
+            ' '*) printf '%s' "${utt13o_color_quote}" >&2 ;;
             *)  ;;
         esac
         printf "%s${utt13o_color_default}\n\n" "${utt13o_paragraph}" >&2
@@ -141,8 +141,7 @@ unittest__test_files() {
         fi
      } |
      while read -r unittest_dir; do
-         find "${unittest_dir:-./}" -path "*${unittest_dir:-tests/}*" -name 'test_*.sh' 2>/dev/null
-         if [ $? -gt 0 ]; then
+         if ! find "${unittest_dir:-./}" -path "*${unittest_dir:-tests/}*" -name 'test_*.sh' 2>/dev/null; then
              unittest__print_debug 'TESTS NOT FOUND' \
                  "I was looking for 'test_*.sh' files inside '${unittest_dir:-tests/}' directory using:" \
                  "    $ find \"${unittest_dir:-./}\" -path \"*${unittest_dir:-tests/}*\" -name 'test_*.sh' -print" \
@@ -170,15 +169,16 @@ unittest__run() {
     # prefix: utt8r_
 
     UNITTEST_STATUS=0
-    for utt8r_testfile in $(cat -); do
+    while read -r utt8r_testfile; do
         (
-            utt8r_beforeAll=$(sed -n 's/^[ \t]*\(beforeAll\)[ \t]*(.*/\1/p' ${utt8r_testfile})
-            utt8r_afterAll=$(sed -n 's/^[ \t]*\(afterAll\)[ \t]*(.*/\1/p' ${utt8r_testfile})
-            utt8r_beforeEach=$(sed -n -e 's/^[ \t]*\(beforeEach\)[ \t]*(.*/\1/p' -e 's/^[ \t]*\(setUp\)[ \t]*(.*/\1/p' ${utt8r_testfile})
-            utt8r_afterEach=$(sed -n -e 's/^[ \t]*\(afterEach\)[ \t]*(.*/\1/p' -e 's/^[ \t]*\(tearDown\)[ \t]*(.*/\1/p' ${utt8r_testfile})
-            utt8r_tests=$(sed -n 's/^[ \t]*\(x\{0,1\}test_[^(]*\)(.*/\1/p' ${utt8r_testfile})
+            utt8r_beforeAll=$(sed -n 's/^[ \t]*\(beforeAll\)[ \t]*(.*/\1/p' "${utt8r_testfile}")
+            utt8r_afterAll=$(sed -n 's/^[ \t]*\(afterAll\)[ \t]*(.*/\1/p' "${utt8r_testfile}")
+            utt8r_beforeEach=$(sed -n -e 's/^[ \t]*\(beforeEach\)[ \t]*(.*/\1/p' -e 's/^[ \t]*\(setUp\)[ \t]*(.*/\1/p' "${utt8r_testfile}")
+            utt8r_afterEach=$(sed -n -e 's/^[ \t]*\(afterEach\)[ \t]*(.*/\1/p' -e 's/^[ \t]*\(tearDown\)[ \t]*(.*/\1/p' "${utt8r_testfile}")
+            utt8r_tests=$(sed -n 's/^[ \t]*\(x\{0,1\}test_[^(]*\)(.*/\1/p' "${utt8r_testfile}")
 
-            . ${utt8r_testfile}
+			# shellcheck source=/dev/null
+            . "${utt8r_testfile}"
             ${utt8r_beforeAll}
             for _current_testcase in ${utt8r_tests}; do
                 UNITTEST_CURRENT="${utt8r_testfile#./}:${_current_testcase}"
@@ -217,7 +217,7 @@ unittest__run() {
 
 {
     # color output by default: on supported terminals when NO_COLOR is not set
-    if [ -z "${NO_COLOR}" ] && [ $(tput colors) -lt 8 ]; then
+    if [ -z "${NO_COLOR}" ] && [ "$(tput colors)" -lt 8 ]; then
         NO_COLOR='YES'
     fi
 
@@ -242,7 +242,7 @@ unittest__run() {
                     ;;
 
                 -v|--version)
-                    printf 'unittest 22.0\n' >&2
+                    printf 'unittest 23.0\n' >&2
                     exit 0
                     ;;
                 *)  # specified directory/file

commit 5d055f4c91b4957a5787b03cd8a5261de245fac1

fix: Invalid format of debug message
Change efdc06f introduced bug: indented debug description prints escaped
characters.

I'm not sure if `%b` format is portable, so I disable shellcheck rule. I also
introduce CLICOLOR_FORCE env var, for testing purposes (more info:
https://bixense.com/clicolors/).

Fixes: #9

unittest
@@ -21,7 +21,7 @@ unittest__print_result() {
     utt9t_color_location=''
     utt9t_color_status=''
 
-    if [ -t 1 ] && [ -z "${NO_COLOR}" ]; then  # stdout is interactive terminal
+    if { [ -n "${CLICOLOR_FORCE}" ] && [ "${CLICOLOR_FORCE}" -ne 0 ]; } || { [ -t 1 ] && [ -z "${NO_COLOR}" ]; }; then  # stdout is interactive terminal
         utt9t_color_default='\033[0m'
         case $2 in
             PASS)  # location: default; status: green
@@ -52,7 +52,7 @@ unittest__print_debug() {
     utt13o_color=''
     utt13o_color_default=''
 
-    if [ -t 2 ] && [ -z "${NO_COLOR}" ]; then  # stderr is interactive terminal
+    if { [ -n "${CLICOLOR_FORCE}" ] && [ "${CLICOLOR_FORCE}" -ne 0 ]; } || { [ -t 2 ] && [ -z "${NO_COLOR}" ]; }; then  # stderr is interactive terminal
         utt13o_color='\033[34m'  # blue
         utt13o_color_default='\033[0m'
         utt13o_color_quote='\033[37m'  # gray
@@ -62,8 +62,9 @@ unittest__print_debug() {
 
     shift 1
     for utt13o_paragraph in "$@"; do
+        # shellcheck disable=SC2059
         case ${utt13o_paragraph} in
-            ' '*) printf '%s' "${utt13o_color_quote}" >&2 ;;
+            ' '*) printf "${utt13o_color_quote}" >&2 ;;
             *)  ;;
         esac
         printf "%s${utt13o_color_default}\n\n" "${utt13o_paragraph}" >&2
@@ -242,7 +243,7 @@ unittest__run() {
                     ;;
 
                 -v|--version)
-                    printf 'unittest 23.0\n' >&2
+                    printf 'unittest 23.1\n' >&2
                     exit 0
                     ;;
                 *)  # specified directory/file

tests/test_output.sh
@@ -1,10 +1,33 @@
 #!/bin/sh
 
+# shellcheck disable=SC2030,SC2031
+
+#
+# PRINT LOCATION
+#
 
 test_status_pass() {
     result=$(unittest__print_result 'test location' 'PASS')
 
     test $? -eq 0 && test "${result}" = 'test location	PASS'
+}
+
+test_status_pass_color() {
+    result=$(export CLICOLOR_FORCE=1; unittest__print_result 'Location' 'PASS' | awk '/\033/')
+
+    test $? -eq 0 && test -n "${result}"
+}
+
+test_status_pass_nocolor() {
+    result=$(export CLICOLOR_FORCE=0; unittest__print_result 'Location' 'PASS' | awk '/\033/')
+
+    test $? -eq 0 && test -z "${result}"
+}
+
+test_status_pass_nocolor_empty() {
+    result=$(export CLICOLOR_FORCE=; unittest__print_result 'Location' 'PASS' | awk '/\033/')
+
+    test $? -eq 0 && test -z "${result}"
 }
 
 test_status_fail() {
@@ -13,10 +36,34 @@ test_status_fail() {
     test $? -eq 0 && test "${result}" = 'failed test location	FAIL'
 }
 
+test_status_fail_color() {
+    result=$(export CLICOLOR_FORCE=1; unittest__print_result 'Location' 'FAIL' | awk '/\033/')
+
+    test $? -eq 0 && test -n "${result}"
+}
+
+test_status_fail_nocolor() {
+    result=$(export CLICOLOR_FORCE=0; unittest__print_result 'Location' 'FAIL' | awk '/\033/')
+
+    test $? -eq 0 && test -z "${result}"
+}
+
 test_status_skip() {
     result=$(unittest__print_result 'skipped test location' 'SKIP')
 
     test $? -eq 0 && test "${result}" = 'skipped test location	SKIP'
+}
+
+test_status_skip_color() {
+    result=$(export CLICOLOR_FORCE=1; unittest__print_result 'Location' 'SKIP' | awk '/\033/')
+
+    test $? -eq 0 && test -n "${result}"
+}
+
+test_status_skip_nocolor() {
+    result=$(export CLICOLOR_FORCE=0; unittest__print_result 'Location' 'SKIP' | awk '/\033/')
+
+    test $? -eq 0 && test -z "${result}"
 }
 
 test_status_unknown() {
@@ -25,3 +72,43 @@ test_status_unknown() {
     test $? -eq 0 && test "${result}" = 'other test location	OUCH'
 }
 
+test_status_unknown_color() {
+    result=$(export CLICOLOR_FORCE=1; unittest__print_result 'Location' 'OUCH' | awk '/\033/')
+
+    test $? -eq 0 && test -n "${result}"
+}
+
+test_status_unknown_nocolor() {
+    result=$(export CLICOLOR_FORCE=0; unittest__print_result 'Location' 'OUCH' | awk '/\033/')
+
+    test $? -eq 0 && test -z "${result}"
+}
+
+#
+# PRINT DEBUG
+#
+
+test_debug() {
+    expected=$(printf '\n-- PROBLEM CLASS\n\nFirst paragraph\n\n    source code\n\nLast paragraph')
+    result=$(unittest__print_debug 'PROBLEM CLASS' 'First paragraph' '    source code' 'Last paragraph' 2>&1)
+
+    test $? -eq 0 && test "${result}" = "${expected}"
+}
+
+test_debug_color() {
+    result=$(export CLICOLOR_FORCE=1; unittest__print_debug 'SOME ERROR' 'I found error in:' '    $ false' 2>&1 | awk '/\033/')
+
+    test $? -eq 0 && test -n "${result}"
+}
+
+test_debug_nocolor() {
+    result=$(export CLICOLOR_FORCE=0; unittest__print_debug 'SOME ERROR' 'I found error in:' '    $ false' 2>&1 | awk '/\033/')
+
+    test $? -eq 0 && test -z "${result}"
+}
+
+test_debug_nocolor_empty() {
+    result=$(export CLICOLOR_FORCE=; unittest__print_debug 'SOME ERROR' 'I found error in:' '    $ false' 2>&1 | awk '/\033/')
+
+    test $? -eq 0 && test -z "${result}"
+}