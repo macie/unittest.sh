0d7fd84cecbcacaabc7a1dfd2fef0254ea94cd69
macie <macie@users.noreply.github.com>
2023-07-18 15:32:56 +0200

    fix: Better color support detection
    Color support detction is based on TERM environmental variable. On Github
    Actions CI environments TERM is not set which generates error messages.
    We should assume, that this kind of terminal is a basic one (dumb terminal).
    
    Fixes: #11

--- tests/test_regression.sh
+++ tests/test_regression.sh
@@ -4,3 +4,8 @@ test_issue8() {
 #shellcheck disable=SC2034,SC2116
 test_var=$(echo 123)
 }
+
+test_issue11() {
+    # unittest version message and tput errors are written to stderr
+    test "$(TERM= ./unittest -v 2>&1 | wc -l)" -eq 1
+}
